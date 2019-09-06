5b9e9da9848cd59533ff38ad81855b5240462f3a
Arnaldo Souza <arnaldoeloi2@gmail.com>
2019-09-06 11:42:28 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const {prefix, token} = require('./config.json');
+
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login(token);
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+
+    console.log(command);
+
+    if(command === 'args-info'){
+        if(!args.length){
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }else if (args[0] === 'foo'){
+            return message.channel.send('bar');
+        }
+
+        message.channel.send(`First argument:  ${args[0]}\n`);
+        message.channel.send(`Command name:  ${command}\nArguments: ${args}`);
+    }
+    
+});
